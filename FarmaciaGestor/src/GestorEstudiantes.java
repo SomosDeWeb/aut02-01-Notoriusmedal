c802217cda492db16be019e512865bfdf12109bd
Notoriusmedal <diegomartinmarrero15@gmail.com>
2025-10-06 19:14:43 +0100

    Creación de la clase principal con método main, lo siento profe cree el commit pensando que era el de farmacia y lo deje asi

---
+++ FarmaciaGestor/src/GestorEstudiantes.java
@@ -0,0 +1,7 @@
+public class GestorEstudiantes {
+
+    public static void main(String[] args) {
+        System.out.println("=== Gestor de Estudiantes ===");
+        System.out.println("Programa iniciado correctamente.");
+    }
+}
